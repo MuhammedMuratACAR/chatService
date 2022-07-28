205d2e54a57fb915f71edcfc0fe17b895888771c
Muhammed Murat ACAR <muhammedmuratacarr@gmail.com>
2022-07-28 09:26:35 +0300

    chatService
    C# Socket Chat Service

---
+++ client/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        public void msg(string mesg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            var text = textBox2.Text;
+            byte[] outStream = System.Text.Encoding.UTF8.GetBytes(text.ToString());
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            //serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            serverStream.Read(inStream, 0, inStream.Length);
+            string returndata = System.Text.Encoding.UTF8.GetString(inStream) + "" + textBox2.Text;
+            msg("Data from Server : " + returndata);
+
+            textBox2.Text = "";
+        }
+
+       
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverSocket = new TcpListener(IPAddress.Any, 8888);
+            //TcpListener serverSocket = new TcpListener(8888);
+
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started");
+
+            counter = 0;
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> " + "Client No:" + Convert.ToString(counter) + " started!");
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+        }
+    }
+
+    //Class to handle each client request separatly
+    public class handleClinet
+    {
+        TcpClient clientSocket;
+        string clNo;
+        public void startClient(TcpClient inClientSocket, string clineNo)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    //networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    dataFromClient = System.Text.Encoding.UTF8.GetString(bytesFrom);
+                    //dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> " + "From client - " + clNo + " " + dataFromClient);
+
+                    rCount = Convert.ToString(requestCount);
+                    serverResponse = "Server to clinet(" + clNo + ") " + rCount + " " + dataFromClient;
+                    sendBytes = Encoding.UTF8.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString());
+                }
+            }
+        }
+    }
+}
